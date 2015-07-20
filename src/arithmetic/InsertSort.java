e59863e3454293d457e5b95b09e6b38620d51561
hmz <hanmz@shinemo.com>
2015-07-20 17:38:47 +0800

    second commit

---
+++ src/arithmetic/InsertSort.java
@@ -0,0 +1,39 @@
+package arithmetic;
+
+public class InsertSort {
+	public static int count = 0;  
+
+	public static void main(String[] args) {  
+
+		int[] data = new int[] { 5, 3, 6, 2, 1, 9, 4, 8, 7 };  
+		print(data);  
+		insertSort(data);  
+		print(data);  
+
+	}  
+
+	public static void insertSort(int[] data) {  
+		for (int i = 1; i < data.length; i++) {  
+			// 缓存i处的元素值  
+			int tmp = data[i];  
+			if (data[i] < data[i - 1]) {  
+				int j = i - 1;  
+				// 整体后移一格  
+				while (j >= 0 && data[j] > tmp) {  
+					data[j + 1] = data[j];  
+					j--;  
+				}  
+				// 最后将tmp插入合适的位置  
+				data[j + 1] = tmp;  
+				print(data);  
+			}  
+		}  
+	}  
+
+	public static void print(int[] data) {  
+		for (int i = 0; i < data.length; i++) {  
+			System.out.print(data[i] + "\t");  
+		}  
+		System.out.println();  
+	}  
+}
---
+++ src/arithmetic/SelectSort.java
@@ -0,0 +1,41 @@
+package arithmetic;
+
+public class SelectSort {
+	public static void main(String[] args) {  
+		int[] data = new int[] { 5, 3, 6, 2, 1, 9, 4, 8, 7 };  
+		print(data);  
+		selectSort(data);  
+		print(data);  
+	}  
+
+	public static void swap(int[] data, int i, int j) {  
+		if (i == j) {  
+			return;  
+		}  
+		data[i] = data[i] + data[j];  
+		data[j] = data[i] - data[j];  
+		data[i] = data[i] - data[j];  
+	}  
+
+	public static void selectSort(int[] data) {  
+		for (int i = 0; i < data.length - 1; i++) {  
+			int minIndex = i; // 记录最小值的索引  
+			for (int j = i + 1; j < data.length; j++) {  
+				if (data[j] < data[minIndex]) {  
+					minIndex = j; // 若后面的元素值小于最小值,将j赋值给minIndex  
+				}  
+			}  
+			if (minIndex != i) {  
+				swap(data, i, minIndex);  
+				print(data);  
+			}  
+		}  
+	}  
+
+	public static void print(int[] data) {  
+		for (int i = 0; i < data.length; i++) {  
+			System.out.print(data[i] + "\t");  
+		}  
+		System.out.println();  
+	}  
+}
